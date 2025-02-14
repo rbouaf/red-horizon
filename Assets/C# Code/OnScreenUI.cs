cd64ef200f6bfc21e6e6a735c662c301d0c84e99
karltchak <47184955+karltchakmakian@users.noreply.github.com>
2025-02-13 19:30:57 -0500

    Rover GUI Update
    Added Speed + Coords to GUI.

---
+++ Assets/C# Code/OnScreenUI.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnScreenUI : MonoBehaviour
+{
+    public Rigidbody roverRigidbody;
+    public TMPro.TextMeshProUGUI speedText;
+
+    public TMPro.TextMeshProUGUI coords;
+
+
+    void Update()
+    {
+        UpdateCoords();
+    }
+
+    void FixedUpdate(){
+         UpdateSpeed();
+    }
+
+     void UpdateSpeed()
+    {
+       Vector3 flatVelocity = Vector3.ProjectOnPlane(roverRigidbody.linearVelocity, Vector3.up);
+            float horizontalSpeed = flatVelocity.magnitude;
+            float speedInKmH = horizontalSpeed * 3.6f;
+
+
+            /*if (speedInKmH > 1 && speedInKmH < 50){
+                speedInKmH = 0.10;
+            }
+            else{
+                speedInKmH = 0.14;
+            }
+            */
+            
+
+            speedText.text = "Average Speed: " + speedInKmH + " km/h";
+    }
+
+    void UpdateCoords(){
+      Vector3 pos = roverRigidbody.position;
+     
+        coords.text = "X: " + pos.x.ToString("F2") + 
+                        " Y: " + pos.y.ToString("F2") + 
+                        " Z: " + pos.z.ToString("F2");
+        
+    }
+}
