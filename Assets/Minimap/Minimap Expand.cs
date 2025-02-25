515d2fae2f7d295425079042938b8e2d02f9317d
karltchak <47184955+karltchakmakian@users.noreply.github.com>
2025-02-24 22:32:22 -0500

    Basic Color and Design + Minimap expand button (V1)
    Added the following:
    1 - Basic button colours.
    2- Bottom menu bar with various functionalities.
    3- Minimap expand button (V1 - Still in progress).

---
+++ Assets/Minimap/Minimap Expand.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+
+public class MinimapExpand : MonoBehaviour
+{
+    public RectTransform minimapRectTransform; 
+    public Camera minimapCamera;         
+    public Vector2 collapsedSize = new Vector2(334, 270);  
+    public Vector2 expandedSize = new Vector2(1920, 1080); 
+    private bool isExpanded = false;        
+
+    private Vector2 originalPosition;
+
+    void Start()
+    {
+        originalPosition = minimapRectTransform.anchoredPosition;  
+    }
+
+    public void ToggleMinimap()
+    {
+        if (isExpanded)
+        {
+            CollapseMinimap();
+        }
+        else
+        {
+            ExpandMinimap();
+        }
+
+        isExpanded = !isExpanded;
+    }
+
+    private void ExpandMinimap()
+    {
+        minimapRectTransform.anchoredPosition = new Vector2(0, 0);
+        minimapRectTransform.sizeDelta = expandedSize;
+        minimapCamera.orthographicSize = 10f;
+    }
+
+    private void CollapseMinimap()
+    {
+        minimapRectTransform.anchoredPosition = originalPosition;
+        minimapRectTransform.sizeDelta = collapsedSize;
+        minimapCamera.orthographicSize = 5f;
+    }
+}
