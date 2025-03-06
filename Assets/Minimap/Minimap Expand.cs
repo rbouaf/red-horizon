989da050f2dd2be554985ff80de3e64309e7178f
karltchak <47184955+karltchakmakian@users.noreply.github.com>
2025-03-06 17:33:29 -0500

    Full-Screen Map V1
    V1 of full screen map.

--- Assets/Minimap/Minimap Expand.cs
+++ Assets/Minimap/Minimap Expand.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 
 public class MinimapExpand : MonoBehaviour
 {
-    /*
+    
     public RectTransform minimapRectTransform; 
+    public GameObject test;
+    public Transform playerOnMap;
     public Camera minimapCamera;         
-    public Vector2 collapsedSize = new Vector2(334, 270);  
-    public Vector2 expandedSize = new Vector2(1920, 1080); 
+    public Vector2 collapsedSize = new Vector2(350, 280);  
+    public Vector2 expandedSize = new Vector2(3000, 3000); 
+    public Vector2 expandedSizePlayer = new Vector2(100, 100); 
     private bool isExpanded = false;        
 
     private Vector2 originalPosition;
@@ -33,16 +36,26 @@ public class MinimapExpand : MonoBehaviour
 
     private void ExpandMinimap()
     {
-        minimapRectTransform.anchoredPosition = new Vector2(0, 0);
+        minimapRectTransform.anchoredPosition = new Vector2(960, -500);
         minimapRectTransform.sizeDelta = expandedSize;
-        minimapCamera.orthographicSize = 10f;
+        minimapCamera.orthographicSize = 50f;
+        //playerOnMap.localScale = new Vector3(200,200,200);
+
+        int myNewLayer = LayerMask.NameToLayer("Player Minimap");
+        test.layer = myNewLayer;
+        
     }
 
     private void CollapseMinimap()
     {
         minimapRectTransform.anchoredPosition = originalPosition;
         minimapRectTransform.sizeDelta = collapsedSize;
-        minimapCamera.orthographicSize = 5f;
+        minimapCamera.orthographicSize = 10f;
+
+        int myNewLayer = LayerMask.NameToLayer("Full-Screen Map");
+        test.layer = myNewLayer;
+
+
     }
-    */
+    
 }
