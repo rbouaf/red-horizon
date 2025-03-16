3fbf306424ec003709bb2ef03a1468fdb56ce88a
karltchak <47184955+karltchakmakian@users.noreply.github.com>
2025-03-15 21:48:42 -0400

    Fixed Small Changes

--- Assets/KarlScripts/OnScreenUI.cs
+++ Assets/KarlScripts/OnScreenUI.cs
@@ -9,10 +9,13 @@ public class OnScreenUI : MonoBehaviour
     public TMPro.TextMeshProUGUI coords;
 
 
+
     void Update()
     {
         UpdateCoords();
     }
+
+
 
     void FixedUpdate(){
          UpdateSpeed();
@@ -22,22 +25,14 @@ public class OnScreenUI : MonoBehaviour
     {
        Vector3 flatVelocity = Vector3.ProjectOnPlane(roverRigidbody.linearVelocity, Vector3.up);
             float horizontalSpeed = flatVelocity.magnitude;
-            float speedInKmH = horizontalSpeed * 3.6f;
+            float speedInKmH = horizontalSpeed * 3.6f ;
 
-
-            // MaxSpeed Perseverance: 0.09 mph
-
-            /*if (speedInKmH > 1 && speedInKmH < 50){
-                speedInKmH = 0.10;
-            }
-            else{
-                speedInKmH = 0.14;
-            }
-            */
-            
 
             speedText.text = "Average Speed: " + speedInKmH + " km/h";
     }
+
+
+
 
     void UpdateCoords(){
       Vector3 pos = roverRigidbody.position;
