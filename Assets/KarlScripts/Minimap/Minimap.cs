9fc1f2bd252d15885549483e20bc5aff804e744c
karltchak <47184955+karltchakmakian@users.noreply.github.com>
2025-04-04 13:45:17 -0400

    Minimap Fixed

--- Assets/KarlScripts/Minimap/Minimap.cs
+++ Assets/KarlScripts/Minimap/Minimap.cs
@@ -5,12 +5,16 @@ public class Minimap : MonoBehaviour
     public GameObject player;
     public Vector3 offset = new Vector3(0, 40, 0);
 
+    public float fixedHeight = 40f;
+
+
     void Start(){
         player =  GameObject.FindGameObjectWithTag("Player");
     }
 
     void LateUpdate(){
         Vector3 playerPosition = player.transform.position;
-        transform.position = new Vector3(playerPosition.x, playerPosition.y + offset.y, playerPosition.z);
+        transform.position = new Vector3(playerPosition.x, fixedHeight, playerPosition.z);
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f);
     }
 }
---
+++ Assets/KarlScripts/Minimap/RedDot.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RedDot : MonoBehaviour
+{
+
+    public Transform rover;
+    public float offsetY = 10f;  
+
+    void LateUpdate()
+    {
+        if (rover == null) return;
+
+       transform.position = new Vector3(rover.position.x, offsetY, rover.position.z); 
+    }
+    
+}
